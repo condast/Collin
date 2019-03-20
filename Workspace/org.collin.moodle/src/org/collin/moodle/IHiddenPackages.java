322e5ad36dfcc318c011d22f384a85c87a0e510e
Condast <info@condast.com>
2019-03-20 11:21:32 +0100

    link to vapid generator added

--- Workspace/org.collin.moodle/src/org/collin/moodle/IHiddenPackages.java
+++ Workspace/org.collin.moodle/src/org/collin/moodle/IHiddenPackages.java
@@ -6,6 +6,11 @@ import org.collin.core.data.*;
 import org.collin.core.graph.*;
 import org.collin.core.task.*;
 
+//https://tools.reactpwa.com/vapid?email=info%40eetmee.nl
+//"subject": "mailto:info@eetmee.nl",
+//"publicKey": "BOxnF300N9zzU2yvOc0YcsXDAjoaqiDjaCWYF9bBjgF8_4od4J1VVtj1S9uGFrfVbV2kDhdDWCKFyWgFNWrPgzw",
+//"privateKey": "qVFcH4iovYSr-HnUUkvpu9kA8v7djmKLzruBi9UK6t4"
+
 @SuppressWarnings("unused")
 public interface IHiddenPackages {
 
